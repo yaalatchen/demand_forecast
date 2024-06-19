66f287eada37988981f5fab4601ebf0a88ae1d86
yczohar <yczohar@brightsourceenergy.com>
2024-06-19 10:21:21 +0300

    try install git

--- training/tsmixer_trainig_main.py
+++ training/tsmixer_trainig_main.py
@@ -2,13 +2,18 @@ import random
 import numpy as np
 import pandas as pd
 import torch
-import git
+import subprocess
+import sys
+
 
 
 
 
 if __name__ =='__main__':
 
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "python-git"])
+
+    import git
     g = git.Repo('.')
     for submodule in g.submodules:
         submodule.update(init=True)
